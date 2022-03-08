beaeeb4f058196451ab83041b2dcbe28d5a0a89d
Shubham Yadav <luckyshubham16@gmail.com>
2022-03-08 19:35:30 +0530

    first commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,23 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+)
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+
+    return accounts.add(config["wallets"]["from_key"])
